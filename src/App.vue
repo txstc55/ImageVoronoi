f42e28670cb0590ff37390b587f05166f4c1faab
Xuan Tang <txstc55@gmail.com>
2020-08-04 18:06:11 -0400

    deploy

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">About</router-link> | 
-      <router-link to="/imagevoronoi">Image Voronoi</router-link>
+      <router-link to="/IV">Image Voronoi</router-link>
     </div>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
   {
-    path: "/ImageVoronoi",
+    path: "/IV",
     name: "Image Voronoi",
     component: IV
   },
