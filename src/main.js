18d1e6eba3b827b25fa4fff6646c46d077688d03
Xuan Tang <txstc55@gmail.com>
2020-08-04 22:35:14 -0400

    add google analytics

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
+Vue.use(VueAnalytics, {
+  id: 'UA-170913292-2',
+  router
+});
 
 new Vue({
   router,
